27e27d85a892a95ffaf91aca4c40d74c1981e79c
KIERON MEAD <1734735@student.fife.ac.uk>
2018-03-07 10:09:41 +0000

    added new sprites and added logging player script

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public string toPrintclass = "Damn World";
+
+
+	// Use this for initialization
+	void Start () {
+		string toPrint = "Bye Bye variable";
+		Debug.Log (toPrintclass);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
