e42ea74023f5a50a75aa1652e9cf92a78ebda978
KIERON MEAD <1734735@student.fife.ac.uk>
2018-03-21 09:48:23 +0000

    got mario to move through key press

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -7,8 +7,9 @@ public class player : MonoBehaviour {
 	public float speed = 5;
 	public float health = 100;
 	public float invulnerableDuration = 1;
-
+	public float blinkDuration = 0.25f;
 	private float invulnerableEndTime = 0;
+	public float blinkEndTime = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -17,11 +18,36 @@ public class player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D> ();
-		ourRigidbody.velocity = Vector2.right*speed;
+		//ourRigidbody.velocity = Vector2.right*speed;
+
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		//get current Velocity from physics system
+		Vector2 velocity = ourRigidbody.velocity;
+
+		//set velocity based on input and speed value
+		velocity.x = horizontal * speed;
+
+		//putting this velocity back into physics system
+		ourRigidbody.velocity = velocity;
+
+
 
 
 		SpriteRenderer ourSprite = GetComponent<SpriteRenderer> ();
-		ourSprite.enabled = false
+
+		if (Time.time >= invulnerableEndTime) {
+
+			ourSprite.enabled = true;
+		} else {
+
+			if (Time.time >= blinkEndTime) {
+				ourSprite.enabled = !ourSprite.enabled;
+
+				blinkEndTime = Time.time + blinkDuration;
+			}
+		}
+		//ourSprite.enabled = false
 }
 
 	public void Damage(float damageToDeal)
